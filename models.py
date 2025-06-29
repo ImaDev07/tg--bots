a6b00a10d5ffc00466858b05b06b42edd7a871c7
ImaDev <luna.dev@mail.ru>
2025-06-29 23:12:30 +0300

    models.py

--- models.py
+++ models.py
@@ -26,34 +26,3 @@ def search_user(user_id):
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM book WHERE user_id = ?', (user_id,))
         return cursor.fetchall()
-
-
-# import sqlite3
-#
-# def create_data(user_id, date, time, people, place):
-#     conn = sqlite3.connect('booking.db')
-#     cur = conn.cursor()
-#     cur.execute('''
-#         create table if not exists bookings (
-#             id integer primary key autoincrement,
-#             user_id integer,
-#             date text,
-#             time text,
-#             people integer,
-#             place text
-#         )
-#     ''')
-#     cur.execute('''
-#         insert into bookings (user_id, date, time, people, place)
-#         values (?, ?, ?, ?, ?)
-#     ''', (user_id, date, time, people, place))
-#     conn.commit()
-#     conn.close()
-#
-# def search_by_user(user_id):
-#     conn = sqlite3.connect('booking.db')
-#     cur = conn.cursor()
-#     cur.execute('select * from bookings where user_id = ?', (user_id,))
-#     results = cur.fetchall()
-#     conn.close()
-#     return results
